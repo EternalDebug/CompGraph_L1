a5e7a706cadfb06c8b496b10f1e62fe6a92d2e6f
EternalDebug <71968876+EternalDebug@users.noreply.github.com>
2023-06-17 01:49:02 +0300

    Add files via upload

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Laba1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(429, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(594, 25);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(388, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(20, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "От";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(553, 30);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(22, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "До";
+			this.label2.Click += new System.EventHandler(this.Label2_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(510, 79);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(119, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Нарисуй мне небо";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(73, 26);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 5;
+			this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(272, 27);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 6;
+			this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(0, 28);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(67, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Номер ф-ии";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(191, 30);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(75, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Детализация";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Laba1
+{
+	public partial class Form1 : Form
+	{
+		private Graphics g;
+
+		public delegate double Fnc(double x);
+
+		Fnc func;
+		double a = 0;
+		double b = 0;
+		int det = 0;
+		double h = 0;
+		double sootnX = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+			g = this.CreateGraphics();
+		}
+
+		//1
+		public double sx(double x)
+		{
+			return Math.Sin(x);
+		}
+
+		//2
+		public double xq(double x)
+		{
+			return x*x;
+		}
+
+		//3
+		public double third(double x)
+		{
+			return x * x * x;
+		}
+
+		private void Label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			//g.Clear(Color.FromArgb(0, 0, 0, 0));
+			a = double.Parse(textBox1.Text);
+			b = double.Parse(textBox2.Text);
+			det = int.Parse(textBox4.Text);
+			h = (b - a) / det;
+			sootnX = Width / (b - a);
+
+
+			Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+			//g.DrawLine(pen, 0, Height / 2, Width, Height / 2);
+			if (a<0 && b >0)
+				g.DrawLine(pen, Width/2 - (int)(sootnX * (a+b)/2), 0, Width / 2 - (int)(sootnX * (a + b) / 2), this.Height);
+			else
+			{
+				if (a == 0)
+					g.DrawLine(pen, 0, 0, 0, this.Height);
+				else
+				if (b == 0)
+					g.DrawLine(pen, Width, 0, Width, this.Height);
+			}
+			//реализовать параметры смещения осей. Нужен для расчета координат в отрисовке
+
+			DrawThisFunc();
+
+		}
+
+		private void DrawThisFunc()
+		{
+			Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+			//double[] Xpoints = new double[21];
+			//double[] Ypoints = new double[21];
+			List<double> Xpoints = new List<double>();
+			List<double> Ypoints = new List<double>();
+			for (int i = 0; i <= det; i++)
+			{
+				//Xpoints[i] = a + h * i;
+				Xpoints.Add(a + h * i);
+				//Ypoints[i] = func(Xpoints[i]);
+				Ypoints.Add(func(Xpoints[i]));
+			}
+
+			var yMin = Ypoints.Min();
+			var yMax = Ypoints.Max();
+
+			double sootnY = (Height / (yMax - yMin))*0.9;
+
+			if (yMin < 0 && yMax > 0)
+				g.DrawLine(pen, 0, Height/2 + (int)(sootnY * (yMax + yMin) / 2) -41, Width, Height / 2 + (int)(sootnY * (yMax + yMin) / 2) - 41);
+			else
+			{
+				if (yMin == 0)
+					g.DrawLine(pen, 0, Height, Width, Height);
+				else
+				if (b == 0)
+					g.DrawLine(pen, 0, 0, Width, 0);
+			}
+
+			
+			double aa = a;
+			double x = a * sootnX;
+			//double y = func(a) * sootnY;
+			for (int i = 0; i< det; i++)
+			{
+				g.DrawLine(pen, (int)((Xpoints[i] - a)*sootnX) - 8, Height - 41 - (int)((Ypoints[i] - yMin)*sootnY), (int)((Xpoints[i+1] - a) * sootnX) -8, Height- 41 - (int)((Ypoints[i+1] - yMin) * sootnY));
+			}
+		}
+
+		//селектор функции. Очень кустарный
+		private void TextBox3_TextChanged(object sender, EventArgs e)
+		{
+			if (textBox3.Text == "1")
+				func = sx;
+			else
+			if (textBox3.Text == "2")
+				func = xq;
+			else
+			if (textBox3.Text == "3")
+				func = third;
+			else
+				func = third;
+		}
+
+		private void TextBox1_TextChanged(object sender, EventArgs e)
+		{
+			//a = double.Parse(textBox1.Text);
+			//h = (b - a) / det;
+			//sootnX = Width / (b - a);
+		}
+
+		private void TextBox2_TextChanged(object sender, EventArgs e)
+		{
+		//	b = double.Parse(textBox2.Text);
+			//h = (b - a) / det;
+			//sootnX = Width / (b - a);
+		}
+
+		private void TextBox4_TextChanged(object sender, EventArgs e)
+		{
+			//det = int.Parse(textBox4.Text);
+			//h = (b - a) / det;
+			//sootnX = Width / (b - a);
+
+			//Если раскомментить, то будет лажа с парсингом минусов
+		}
+	}
+}
